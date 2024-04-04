e23b8550e9b028426d9becfb9f9edbefb137e868
AntonisDela <antonisdelavogias@icloud.com>
2024-04-04 21:20:18 +0300

    after reading the documentation of leaflet im setting the preferences i want, into the code

--- script.js
+++ script.js
@@ -16,9 +16,6 @@ if (navigator.geolocation)
     function (position) {
       const { latitude } = position.coords;
       const { longitude } = position.coords;
-      console.log(
-        `https://www.google.com/maps/@${latitude},${longitude}?entry=ttu`
-      );
 
       const coords = [latitude, longitude];
       const map = L.map("map").setView(coords, 15);
@@ -28,10 +25,24 @@ if (navigator.geolocation)
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup("A pretty CSS popup.<br> Easily customizable.")
-        .openPopup();
+      map.on("click", function (mapEvent) {
+        console.log(mapEvent);
+        const { lat, lng } = mapEvent.latlng;
+
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: "running-popup",
+            })
+          )
+          .setPopupContent("Workout")
+          .openPopup();
+      });
     },
     function () {
       alert("Could not get your position");
