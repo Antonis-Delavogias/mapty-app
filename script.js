066a5a6a77bfd780afc2f51714aa8acba1039d11
AntonisDela <antonisdelavogias@icloud.com>
2024-04-05 18:19:52 +0300

    implented the  functionality , now above the mini box in the map, the user can see if he did run or cycle and which day/month

--- script.js
+++ script.js
@@ -5,18 +5,18 @@
 class Workout {
   date = new Date();
   id = (Date.now() + "").slice(-10); // a fake method to create ids
-
+  
   constructor(coords, distance, duration) {
     this.coords = coords; // [lat, lng]
     this.distance = distance; //in km
     this.duration = duration; //in min
-    
+      
   }
   _setDescription() {
     // prettier-ignore
     const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
-    this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${[this.date.getMonth()]} ${this.date.getDate()}`
+    
+    this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${months[this.date.getMonth()]} ${this.date.getDate()}`;
   }
 }
 
@@ -195,7 +195,9 @@ class App {
           className: `${workout.type}-popup`,
         })
       )
-      .setPopupContent("workout")
+      .setPopupContent(
+        `${workout.type === "running" ? "🏃‍♂️" : "🚴‍♀️"} ${workout.description}`
+      )
       .openPopup();
   }
 
